f481d84e8efda902733eadffb78c18d684667537
Evan Lavertu <elavertu@rams.colostate.edu>
2019-10-15 13:56:07 -0600

    Regenerated client side

--- Banqi/frontend/src/App.vue
+++ Banqi/frontend/src/App.vue
@@ -1,27 +1,57 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <header>
+      <span>Banqi</span>
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
   </div>
 </template>
 
 <script>
 import Home from './components/Home'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-      Home
+    Home
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #35495E;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
 }
 </style>
--- Banqi/frontend/src/components/Home.vue
+++ Banqi/frontend/src/components/Home.vue
@@ -1,18 +1,34 @@
 <template>
-    <div class="home">
-      <h1 id="title">{{title}}</h1>
-    </div>
+  <div class="home">
+    <h1 id="title">Banqi</h1>
+    <button @click="callHelloApi()">CALL Spring Boot REST backend service</button>
+    <h4>Backend response: {{ backendResponse }}</h4>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home",
-        data() {
-            return {
-                title: 'Banqi'
-            }
-        }
+  import API from '../api';
+  export default {
+    name: "Home",
+    data() {
+      return {
+        msg: "HowTo call REST-Services",
+        backendResponse: [],
+        errors: []
+      }
+    },
+    methods: {
+      callHelloApi() {
+        API.hello().then(response => {
+          this.backendResponse = response.data;
+          console.log(response.data);
+        })
+        .catch(error => {
+          this.errors.push(error);
+        })
+      }
     }
+  }
 </script>
 
 <style scoped>
