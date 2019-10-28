48ebbc101cda4d297118a840c605dd58ee4a6c89
crcoleman <ichicoleman@gmail.com>
2019-10-28 00:48:41 -0600

    Added Square class to the board package. A square stores a piece and a coordinate.

---
+++ Banqi/backend/src/main/java/com/hackersanon/banqi/main/board/Square.java
@@ -0,0 +1,39 @@
+package com.hackersanon.banqi.main.board;
+
+import com.hackersanon.banqi.main.piece.Piece;
+
+public class Square {
+    private Piece storedPiece;
+    private Coordinate coordinate;
+    private boolean occupied = false;
+
+    protected Square(Coordinate coordinate, Piece piece){
+        this.coordinate = coordinate;
+        this.storedPiece = piece;
+        this.occupied = true;
+    }
+
+    protected Square(Coordinate coordinate){
+        this.coordinate = coordinate;
+    }
+
+    public Piece getStoredPiece(){
+        return storedPiece;
+    }
+
+    public Coordinate getCoordinate(){
+        return coordinate;
+    }
+
+    public void setStoredPiece(Piece newPiece){
+        if(!occupied) {
+            this.storedPiece = newPiece;
+            toggleOccupied();
+        }
+        //TODO should square check for occupying piece team and check validity of capture of friendly piece? need to come back. I don't think square should be responsible.
+    }
+
+    private void toggleOccupied(){
+        occupied = !occupied;
+    }
+}
