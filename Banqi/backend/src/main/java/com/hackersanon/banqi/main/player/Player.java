4053b18d31900899ab60282f29345c670e9f709f
crcoleman <ichicoleman@gmail.com>
2019-10-28 01:29:24 -0600

    added isTurn() - returns boolean if it is the players turn. toggleTurn() - sets turn variable to opposite stored value.

--- Banqi/backend/src/main/java/com/hackersanon/banqi/main/player/Player.java
+++ Banqi/backend/src/main/java/com/hackersanon/banqi/main/player/Player.java
@@ -4,11 +4,18 @@ import com.hackersanon.banqi.main.piece.TeamColor;
 
 public class Player {
     private TeamColor team;
-
+    private boolean turn = false;
     public Player(TeamColor color){
         this.team = color;
     }
 
+    public boolean isTurn(){
+        return turn;
+    }
+
+    public void toggleTurn(){
+        turn = !turn;
+    }
     public TeamColor getPlayerTeam(){
         return team;
     }
