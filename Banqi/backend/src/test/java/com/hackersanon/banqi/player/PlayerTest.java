cabb99aaacd26f66fc8048097a27a98c37dd6012
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-10-28 07:20:12 -0600

    Generated Junit tests for player package #70

---
+++ Banqi/backend/src/test/java/com/hackersanon/banqi/player/PlayerTest.java
@@ -0,0 +1,25 @@
+package com.hackersanon.banqi.player;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void isTurn() {
+    }
+
+    @Test
+    void toggleTurn() {
+    }
+
+    @Test
+    void getPlayerTeam() {
+    }
+}
