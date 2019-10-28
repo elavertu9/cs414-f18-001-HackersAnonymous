2329cd73f85771f5372c83596ca02e2a4e33470f
crcoleman <ichicoleman@gmail.com>
2019-10-27 18:04:53 -0600

    skeleton abstract class Piece. #69

---
+++ Banqi/backend/src/main/java/com/hackersanon/banqi/main/piece/Piece.java
@@ -0,0 +1,15 @@
+package com.hackersanon.banqi.main.piece;
+
+public abstract class Piece {
+    boolean faceUp;
+    private int row;
+    private int column;
+
+    public Piece(){
+
+    }
+
+    
+
+
+}
