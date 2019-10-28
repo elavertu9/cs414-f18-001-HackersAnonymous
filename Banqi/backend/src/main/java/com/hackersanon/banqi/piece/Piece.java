17c202ff9bb31751032b573936f54cb52975a562
crcoleman <ichicoleman@gmail.com>
2019-10-28 17:22:19 -0600

    Refactored the Piece enum to PieceAttributes and added a new Piece class which now contains an instance var for the enums: TeamColor and PieceAttributes, and provides the necessary non-static methods the enum could not do alone. #69

--- Banqi/backend/src/main/java/com/hackersanon/banqi/piece/Piece.java
+++ Banqi/backend/src/main/java/com/hackersanon/banqi/piece/Piece.java
@@ -2,28 +2,22 @@ package com.hackersanon.banqi.piece;
 
 import com.hackersanon.banqi.board.Coordinate;
 
-import java.util.ArrayList;
+public class Piece {
+    private final PieceAttributes pieceType;
+    private final TeamColor team;
+    private boolean faceUp = false;
 
-public enum Piece {
-    GENERAL(){
-    },
-    CHARIOT(){
-    },
-    HORSE(){
-    },
-    CANNON(){
-    },
-    ADVISOR(){
-    },
-    MINSTER(){
-    },
-    SOLDIER(){
-    };
+    public Piece(PieceAttributes pieceType, TeamColor color){
+        this.pieceType = pieceType;
+        this.team = color;
+    }
 
-    boolean faceUp = false;
-    private TeamColor teamColor;
+    public String getPieceType() {
+        return pieceType.toString();
+    }
 
-    Piece(){
+    public TeamColor getTeam() {
+        return team;
     }
 
     public boolean flipPiece(){
@@ -34,38 +28,13 @@ public enum Piece {
         }
     }
 
-    public TeamColor getTeam(){
-        return teamColor;
+    public boolean isValidMove(Coordinate origin, Coordinate destination){
+        return pieceType.isValidMove(origin, destination);
     }
+
+
 
     public String toString(){
-        return this.getTeam().name() + this.name();
+        return team.toString()+pieceType.toString();
     }
-
-
-    public void setTeamColor(TeamColor teamColor) {
-        if(this.teamColor == null){
-            this.teamColor = teamColor;
-        }
-    }
-
-    public boolean isValidMove(Coordinate origin, Coordinate destination){
-        return legalMoves(origin).contains(destination);
-    }
-
-    private ArrayList<Coordinate> legalMoves(Coordinate origin){
-        ArrayList<Coordinate> legalMoves = new ArrayList<>();
-        addLegalMove(new Coordinate(origin.getRow()+1,origin.getColumn()), legalMoves);
-        addLegalMove(new Coordinate(origin.getRow()-1,origin.getColumn()), legalMoves);
-        addLegalMove(new Coordinate(origin.getRow(),origin.getColumn()+1), legalMoves);
-        addLegalMove(new Coordinate(origin.getRow(),origin.getColumn()-1), legalMoves);
-        return legalMoves;
-    }
-
-    private static void addLegalMove(Coordinate destination, ArrayList<Coordinate> legalMoves){
-        if(Coordinate.isCoordinateValid(destination.getRow(), destination.getColumn())){
-            legalMoves.add(destination);
-        }
-    }
-
 }
