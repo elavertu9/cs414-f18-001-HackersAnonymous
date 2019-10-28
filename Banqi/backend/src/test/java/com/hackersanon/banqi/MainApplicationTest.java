300c3af6791e197204b9eb84031a2dab56374a59
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-10-28 07:21:20 -0600

    Generated Junit tests for BackendController and MainApplication #70

---
+++ Banqi/backend/src/test/java/com/hackersanon/banqi/MainApplicationTest.java
@@ -0,0 +1,17 @@
+package com.hackersanon.banqi;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void main() {
+    }
+}
