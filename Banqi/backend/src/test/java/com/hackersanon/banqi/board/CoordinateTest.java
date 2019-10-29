82e497a4647bdb76d571217f37415c4df2dd28d7
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-10-28 18:59:08 -0600

    Finished basic verification tests #70

--- Banqi/backend/src/test/java/com/hackersanon/banqi/board/CoordinateTest.java
+++ Banqi/backend/src/test/java/com/hackersanon/banqi/board/CoordinateTest.java
@@ -7,35 +7,57 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CoordinateTest {
 
+    private Coordinate coordinate;
+
     @BeforeEach
     void setUp() {
+        coordinate = new Coordinate(0, 0);
     }
 
     @Test
     void getRow() {
-        Coordinate coordinate = new Coordinate(3,3);
-        assertEquals(3, coordinate.getRow());
+        assertEquals(0, coordinate.getRow());
     }
 
     @Test
     void getColumn() {
-        Coordinate coordinate = new Coordinate(2,7);
-        assertEquals(7, coordinate.getColumn());
+        assertEquals(0, coordinate.getColumn());
     }
 
     @Test
     void setColumn() {
+        assertTrue(coordinate.setColumn(5));
+    }
+
+    @Test
+    void setInvalidColumn() {
+        assertFalse(coordinate.setColumn(-5));
+        assertFalse(coordinate.setColumn(9));
     }
 
     @Test
     void setRow() {
+        assertTrue(coordinate.setRow(2));
+    }
+
+    @Test
+    void setInvalidRow() {
+        assertFalse(coordinate.setRow(-2));
+        assertFalse(coordinate.setRow(4));
     }
 
     @Test
     void isCoordinateValid() {
+        assertTrue(Coordinate.isCoordinateValid(3, 7));
+    }
+
+    @Test
+    void isCoordinateInvalid() {
+        assertFalse(Coordinate.isCoordinateValid(8, -4));
     }
 
     @Test
     void getStringPosition() {
+        assertEquals("a0", coordinate.getStringPosition());
     }
 }
