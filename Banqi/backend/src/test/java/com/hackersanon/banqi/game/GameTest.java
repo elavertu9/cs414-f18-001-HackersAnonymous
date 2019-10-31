cb4baa8092bc2e7676be8a0f6b60c2bf706f1996
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-10-31 17:10:44 -0600

    Generate GameTest file #88

---
+++ Banqi/backend/src/test/java/com/hackersanon/banqi/game/GameTest.java
@@ -0,0 +1,41 @@
+package com.hackersanon.banqi.game;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void isGameOver() {
+    }
+
+    @Test
+    void undoMove() {
+    }
+
+    @Test
+    void getValidMoves() {
+    }
+
+    @Test
+    void getBanqiBoard() {
+    }
+
+    @Test
+    void getPlayers() {
+    }
+
+    @Test
+    void getMoveHistory() {
+    }
+}
