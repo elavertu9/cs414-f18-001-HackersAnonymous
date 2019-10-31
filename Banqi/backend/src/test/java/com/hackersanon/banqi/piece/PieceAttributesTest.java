7e9807d1e6831443e9ed5559cba3cd9499ef3b0e
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-10-31 04:22:47 -0600

    Implemented isValidMove() test #76

--- Banqi/backend/src/test/java/com/hackersanon/banqi/piece/PieceAttributesTest.java
+++ Banqi/backend/src/test/java/com/hackersanon/banqi/piece/PieceAttributesTest.java
@@ -1,5 +1,6 @@
 package com.hackersanon.banqi.piece;
 
+import com.hackersanon.banqi.board.Coordinate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +18,18 @@ class PieceAttributesTest {
 
     @Test
     void isValidMove() {
+        Coordinate origin = new Coordinate(2,2);
+        Coordinate destUp = new Coordinate(3,2);
+        Coordinate destDown = new Coordinate(1, 2);
+        Coordinate destLeft = new Coordinate(2, 1);
+        Coordinate destRight = new Coordinate(2, 3);
+
+        for(PieceAttributes pieceType : PieceAttributes.values())
+        {
+            assertTrue(pieceType.isValidMove(origin, destUp));
+            assertTrue(pieceType.isValidMove(origin, destDown));
+            assertTrue(pieceType.isValidMove(origin, destLeft));
+            assertTrue(pieceType.isValidMove(origin, destRight));
+        }
     }
 }
