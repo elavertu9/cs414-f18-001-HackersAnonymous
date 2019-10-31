ba8048303aae65bd9058f7014e2bd1cd39af9109
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-10-31 07:22:49 -0600

    Implement SquareTest setUp() #79

--- Banqi/backend/src/test/java/com/hackersanon/banqi/board/SquareTest.java
+++ Banqi/backend/src/test/java/com/hackersanon/banqi/board/SquareTest.java
@@ -1,14 +1,24 @@
 package com.hackersanon.banqi.board;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.hackersanon.banqi.piece.Piece;
+import com.hackersanon.banqi.piece.PieceAttributes;
+import com.hackersanon.banqi.piece.TeamColor;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SquareTest {
 
-    @BeforeEach
-    void setUp() {
+    private static Square square;
+    private static Square emptySquare;
+    private static Piece blackCannon;
+
+    @BeforeAll
+    static void setUp() {
+        blackCannon = new Piece(PieceAttributes.CANNON, TeamColor.BLACK);
+        square = new Square(new Coordinate(0, 0), blackCannon);
+        emptySquare = new Square(new Coordinate(0, 0), null);
     }
 
     @Test
