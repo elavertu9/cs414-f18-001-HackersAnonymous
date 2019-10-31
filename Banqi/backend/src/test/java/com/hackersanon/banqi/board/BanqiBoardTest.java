07192c0533fab54239693121491499736b543ac4
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-10-30 18:07:25 -0600

    Merge branch 'master' of /home/jhfitzg/IdeaProjects/cs414-f18-001-HackersAnonymous with conflicts.

---
+++ Banqi/backend/src/test/java/com/hackersanon/banqi/board/BanqiBoardTest.java
@@ -0,0 +1,18 @@
+package com.hackersanon.banqi.board;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BanqiBoardTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void initialize() {
+
+    }
+}
