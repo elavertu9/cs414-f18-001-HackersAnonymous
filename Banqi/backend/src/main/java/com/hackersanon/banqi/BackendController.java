b306bb9415c70f7bcee9bba66f7e297bf4aa5f23
buckledown <ichicoleman@gmail.com>
2019-11-07 19:08:22 -0700

    Add /create endpoint

--- Banqi/backend/src/main/java/com/hackersanon/banqi/BackendController.java
+++ Banqi/backend/src/main/java/com/hackersanon/banqi/BackendController.java
@@ -1,5 +1,8 @@
 package com.hackersanon.banqi;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hackersanon.banqi.game.Game;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +15,22 @@ public class BackendController {
     public String sayHello() {
         return "Hello from the backend!";
     }
-
+    
+    @CrossOrigin(origins = {"http://localhost:8081"})
+    @GetMapping("/api/create")
+    public String startGame() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String jsonString = mapper.writeValueAsString(new Game());
+            return jsonString;
+        }
+        catch (JsonProcessingException e) {
+            System.out.println("Java to JSON conversion fail.");
+        }
+        return "";
+        
+    }
+    
     @CrossOrigin(origins = {"http://localhost:8081"})
     @GetMapping("/api/board")
     public String getBoard() {
@@ -22,7 +40,7 @@ public class BackendController {
      @CrossOrigin(origins = {"http://localhost:8081"})
     @GetMapping("/api/")
     public String executeMove() {//TODO JSON converter: Move Object to JSON?
-
+        
         return null;
     }
 }
