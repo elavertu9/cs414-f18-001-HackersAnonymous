eb2e1e9d9c7148ac8485f9117a701a3406127ceb
crcoleman <ichicoleman@gmail.com>
2019-12-04 03:22:21 -0700

    - Refactored the signature in the repository classes to have the classes available at compile time.

--- Banqi/backend/src/main/java/com/hackersanon/banqi/database/dao/GameDAO.java
+++ Banqi/backend/src/main/java/com/hackersanon/banqi/database/dao/GameDAO.java
@@ -1,9 +1,9 @@
 package com.hackersanon.banqi.database.dao;
 
-import com.hackersanon.banqi.game.Game;
+import com.hackersanon.banqi.database.model.Game;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GameDAO extends IBaseRepository<Game>
+public interface GameDAO extends IBaseRepository<Game, Long>
 {
 }
--- Banqi/backend/src/main/java/com/hackersanon/banqi/database/dao/UserDAO.java
+++ Banqi/backend/src/main/java/com/hackersanon/banqi/database/dao/UserDAO.java
@@ -2,6 +2,6 @@ package com.hackersanon.banqi.database.dao;
 
 import com.hackersanon.banqi.database.model.User;
 
-public interface UserDAO extends IBaseRepository<User>
+public interface UserDAO extends IBaseRepository<User, Long>
 {
 }
