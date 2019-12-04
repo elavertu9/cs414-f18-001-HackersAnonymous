8708718521a148a089548e153879b7eea90a4bb8
crcoleman <ichicoleman@gmail.com>
2019-12-04 03:26:27 -0700

    - Moved BackendController back to base folder. - Fixed small mapping issue.

--- Banqi/backend/src/main/java/com/hackersanon/banqi/BackendController.java
+++ Banqi/backend/src/main/java/com/hackersanon/banqi/BackendController.java
@@ -1,16 +1,14 @@
-package com.hackersanon.banqi.controller;
+package com.hackersanon.banqi;
 
-import com.hackersanon.banqi.board.BanqiBoard;
+import com.hackersanon.banqi.database.model.Board;
+import com.hackersanon.banqi.database.model.Game;
 import com.hackersanon.banqi.database.service.IGameService;
 import com.hackersanon.banqi.database.service.IUserService;
-import com.hackersanon.banqi.game.Game;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 @CrossOrigin(origins = {"http://localhost:8081"})
 public class BackendController
 {
@@ -20,16 +18,16 @@ public class BackendController
     @Autowired
     private IUserService userService;
     
-    @GetMapping("hello")
+    @GetMapping(value = "/game/hello")
     public String sayHello()
     {
         return "Hello from the backend!";
     }
     
     @GetMapping("/game/{gameId}/board")
-    public BanqiBoard getBoard(@PathVariable Long gameId)
+    public Board getBoard(@PathVariable Long gameId)
     {
-        return gameService.findById(gameId).getBanqiBoard();
+        return gameService.findById(gameId).getBoard();
     }
     
     @GetMapping("/game/create")
