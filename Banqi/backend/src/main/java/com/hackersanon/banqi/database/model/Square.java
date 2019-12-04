4ebb055534590eb4aabb6964f4b4b16ed90f2454
crcoleman <ichicoleman@gmail.com>
2019-12-03 17:36:10 -0700

    - Created embeddable Square model for use with hibernate and the database.

--- Banqi/backend/src/main/java/com/hackersanon/banqi/database/model/Square.java
+++ Banqi/backend/src/main/java/com/hackersanon/banqi/database/model/Square.java
@@ -1,5 +1,44 @@
 package com.hackersanon.banqi.database.model;
 
-public class Square extends ModelBase
+
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
+import javax.validation.constraints.NotNull;
+
+@Embeddable
+public class Square
 {
+    @NotNull
+    @Embedded
+    private Piece piece;
+
+    @NotNull
+    @Embedded
+    private Coordinate coordinate;
+
+    public Square() {
+    }
+
+    public Square(Piece piece, Coordinate coordinate){
+        this.piece = piece;
+        this.coordinate = coordinate;
+    }
+
+    public void setPiece(Piece piece){
+        this.piece = piece;
+    }
+
+    public Piece getPiece() {
+        return this.piece;
+    }
+
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public Coordinate getCoordinate(){
+        return this.coordinate;
+    }
+
+
 }
