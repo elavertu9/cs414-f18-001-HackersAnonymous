ac6908c308543ba748a2f1ba2c5d90ed2b7b8813
crcoleman <ichicoleman@gmail.com>
2019-12-04 13:06:30 -0700

    - BanqiBoard now provides static methods that GameServices can call upon to edit the board state represented by the models.

--- Banqi/backend/src/main/java/com/hackersanon/banqi/board/BanqiBoard.java
+++ Banqi/backend/src/main/java/com/hackersanon/banqi/board/BanqiBoard.java
@@ -1,54 +1,47 @@
 package com.hackersanon.banqi.board;
 
-import com.hackersanon.banqi.game.Move;
-import com.hackersanon.banqi.piece.Piece;
+import com.hackersanon.banqi.database.model.Coordinate;
+import com.hackersanon.banqi.database.model.Piece;
+import com.hackersanon.banqi.database.model.Square;
 import com.hackersanon.banqi.piece.TeamColor;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
-import static com.hackersanon.banqi.game.Move.Actions.*;
 import static com.hackersanon.banqi.piece.PieceAttributes.*;
 
 public class BanqiBoard
 {
-    private List<Square> board;
-    
-    
-    private int colDimension = 8;
-    private int rowDimension = 4;
-    private boolean gameOver;
-
+    private static int colDimension = 8;
+    private static int rowDimension = 4;
 
     public BanqiBoard(){
-        this.board = new ArrayList<>();
     }
 
-    public void initialize(){
-        initSquares(initAllPieces());
+    public static Collection<Square> initialize(){
+        return initSquares();
     }
 
-    private void initSquares(ArrayList<Piece> allPieces){
-        
+    private static List<Square> initSquares(){
+        ArrayList<Piece> allPiece = initAllPieces();
+        List<com.hackersanon.banqi.database.model.Square> board = new ArrayList<>();
         for (int i = 0; i<rowDimension;++i){
             for(int j = 0; j<colDimension;++j){
-                board.add(new Square(new Coordinate(i, j), allPieces.remove(0)));
+                board.add(new com.hackersanon.banqi.database.model.Square(allPiece.remove(0), new Coordinate(i, j) ));
             }
         }
+        return board;
     }
 
-    public ArrayList<Piece> initAllPieces(){
-        ArrayList<Piece> allPieces = new ArrayList<>(initTeamPieces(TeamColor.RED));
-        allPieces.addAll(initTeamPieces(TeamColor.BLACK));
+    private static ArrayList<Piece> initAllPieces(){
+        ArrayList<Piece> allPiece = new ArrayList<>(initTeamPieces(TeamColor.RED));
+        allPiece.addAll(initTeamPieces(TeamColor.BLACK));
         for(int i=0;i<5;++i){
-            Collections.shuffle(allPieces);
+            Collections.shuffle(allPiece);
         }
-        return allPieces;
+        return allPiece;
     }
 
-    private ArrayList<Piece> initTeamPieces(TeamColor color){
+    private static ArrayList<Piece> initTeamPieces(TeamColor color){
         return new ArrayList<>(Arrays.asList( new Piece(GENERAL, color),
                 new Piece(CHARIOT, color), new Piece(CHARIOT, color),
                 new Piece(HORSE, color), new Piece(HORSE, color),
@@ -59,49 +52,36 @@ public class BanqiBoard
                 new Piece(SOLDIER, color), new Piece(SOLDIER, color),new Piece(SOLDIER, color)));
     }
 
-    
-    public List<Square> getBoard() {
-        return board;
-    }
-
-    public int getColDimension() {
-        return colDimension;
-    }
-
-    public int getRowDimension() {
-        return rowDimension;
-    }
-
-    public Square getSquare(Coordinate coordinate){
-        if(coordinate.isValid()){
-            return board.get(coordinate.getRow()*rowDimension+coordinate.getColumn());
-        }else{
-            return null; //TODO throw new exception?
-        }
-    }
-
-    public Piece getPieceAt(Coordinate coordinate){
-        if (this.getSquare(coordinate) != null) {
-            return this.getSquare(coordinate).getStoredPiece();
-        }
-        else {
-            return null;
-        }
-    }
+//    public Square getSquare(Coordinate coordinate){
+//        if(coordinate.isValid()){
+//            return board.get(coordinate.getRow()*rowDimension+coordinate.getColumn());
+//        }else{
+//            return null; //TODO throw new exception?
+//        }
+//    }
+//
+//    public Piece getPieceAt(Coordinate coordinate){
+//        if (this.getSquare(coordinate) != null) {
+//            return this.getSquare(coordinate).getStoredPiece();
+//        }
+//        else {
+//            return null;
+//        }
+//    }
 
 
-    public Move makeMove(Move newMove){
-        return makeMove(getSquare(newMove.getOrigin()),getSquare(newMove.getDestination()));
-    }
-
-    public Move makeMove(Square origin, Square destination){
-        Move newMove = new Move(this, origin,destination);
-        if(newMove.getActionType() == TRAVEL || newMove.getActionType() == CAPTURE){
-            return newMove.executeMove(this);
-        }else if(newMove.getActionType() == FLIP){
-            return newMove.executeFlip(this);
-        }
-        return newMove;
-    }
+//    public Move makeMove(Move newMove){
+//        return makeMove(getSquare(newMove.getOrigin()),getSquare(newMove.getDestination()));
+//    }
+//
+//    public Move makeMove(SquareFunctions origin, SquareFunctions destination){
+//        Move newMove = new Move(this, origin,destination);
+//        if(newMove.getActionType() == TRAVEL || newMove.getActionType() == CAPTURE){
+//            return newMove.executeMove(this);
+//        }else if(newMove.getActionType() == FLIP){
+//            return newMove.executeFlip(this);
+//        }
+//        return newMove;
+//    }
 
 }
