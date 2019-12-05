d2eae815fcbfc2c4c51b86bfe87705845f478bac
patrickkeleher <46502198+patrickkeleher@users.noreply.github.com>
2019-12-04 19:08:35 -0700

    Add game piece rules.

--- Banqi/frontend/src/components/BanqiRules.vue
+++ Banqi/frontend/src/components/BanqiRules.vue
@@ -12,37 +12,37 @@
         <tr class="center">
           <td><img src="../images/Pieces/White_Advisor.png"></td>
           <td class="middle">Advisor</td>
-          <td class="middle">Blah</td>
+          <td class="middle">A piece may never move onto a square that is already occupied unless such a move is a legal capture. This is true for all pieces. Can move one space left, right, up, or down. Pieces of the opposite color that can be captured are advisor, elephant, chariot, horse, soldier and cannon.</td>
         </tr>
         <tr class="center">
           <td><img src="../images/Pieces/White_Soldier.png"></td>
           <td class="middle">Soldier</td>
-          <td class="middle">Blah</td>
+          <td class="middle">Can move one space left, right, up, or down. Can capture soliders, general, and cannons of the opposite color.</td>
         </tr>
         <tr class="center">
           <td><img src="../images/Pieces/White_General.png"></td>
           <td class="middle">General</td>
-          <td class="middle">Blah</td>
+          <td class="middle">Can move one space left, right, up, or down. Can capture any other piece of the opposite color but soliders.</td>
         </tr>
         <tr class="center">
           <td><img src="../images/Pieces/White_Elephant.png"></td>
           <td class="middle">Elephant</td>
-          <td class="middle">Blah</td>
+          <td class="middle">Can move one space left, right, up, or down. Can capture elephants, chariots, horses, soldiers, and cannons of the opposite color.</td>
         </tr>
         <tr class="center">
           <td><img src="../images/Pieces/White_Cannon.png"></td>
           <td class="middle">Cannon</td>
-          <td class="middle">Blah</td>
+          <td class="middle">Can move one space left, right, up, or down. Can only capture cannons of the opposite color.</td>
         </tr>
         <tr class="center">
           <td><img src="../images/Pieces/White_Chariot.png"></td>
           <td class="middle">Chariot</td>
-          <td class="middle">Blah</td>
+          <td class="middle">Can move one space left, right, up, or down. Can capture chariots, horses, soldiers, and cannons of the opposite color.</td>
         </tr>
         <tr class="center">
           <td><img src="../images/Pieces/White_Horse.png"></td>
           <td class="middle">Horse</td>
-          <td class="middle">Blah</td>
+          <td class="middle">Can move one space left, right, up, or down. Can capture horses, soldiers, and cannos of the opposite color.</td>
         </tr>
         </tbody>
       </table>
