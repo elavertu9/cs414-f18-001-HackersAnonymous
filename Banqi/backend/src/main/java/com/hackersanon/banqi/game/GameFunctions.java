34c4dbf5fef921ffee1cdfe047e1f7abc4b14bff
crcoleman <ichicoleman@gmail.com>
2019-12-05 16:50:13 -0700

    - renamed game.Game to game.GameFunctions and code cleanup.

--- Banqi/backend/src/main/java/com/hackersanon/banqi/game/GameFunctions.java
+++ Banqi/backend/src/main/java/com/hackersanon/banqi/game/GameFunctions.java
@@ -1,44 +1,23 @@
 package com.hackersanon.banqi.game;
 
-import com.hackersanon.banqi.board.BanqiBoard;
+import com.hackersanon.banqi.board.BoardFunctions;
+import com.hackersanon.banqi.board.InvalidCoordinateException;
+import com.hackersanon.banqi.database.model.Board;
+import com.hackersanon.banqi.database.model.Coordinate;
+import com.hackersanon.banqi.piece.PieceFunctions;
+
+import java.util.ArrayList;
 
 
 public class GameFunctions
 {
-    private BanqiBoard banqiBoard;
 
-
-    public GameFunctions(){
-        banqiBoard = new BanqiBoard();
-    }
-    
-    public void start(){
-        banqiBoard.initialize();
-    }
-    
-    public boolean isGameOver(){
+    public static boolean isGameOver(){
         return false; //TODO need to implement isGameOver()
     }
-//
-//    public ArrayList<Coordinate> getValidMoves(Coordinate origin){
-//        return banqiBoard.getSquare(origin).getStoredPiece().getValidMoveList(origin);
-//    }
-    
-    public void attemptMove(Move newMove) throws GameOverException {
-        if(isGameOver()){
-            throw new GameOverException();
-        }
+
+    public ArrayList<Coordinate> getValidMoves(Board board, Coordinate origin) throws InvalidCoordinateException {
+        return PieceFunctions.getValidMoveList(origin,BoardFunctions.getSquare(board,origin).getPiece());
     }
-//
-//    public void attemptMove(String origin, String destination) throws GameOverException {
-//        Move move = new Move(this.getBanqiBoard(), CoordinateFunctions.convertANtoCoord(origin), CoordinateFunctions.convertANtoCoord(destination));
-//        if (isGameOver()){
-//            throw new GameOverException();
-//        }
-//    }
-    
-    public BanqiBoard getBanqiBoard() {
-        return banqiBoard;
-    }
-    
+
 }
