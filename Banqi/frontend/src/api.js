afa3cd93f0a8ab7a52b4cf8ae56535abd8a9957b
Evan Lavertu <elavertu@rams.colostate.edu>
2019-12-04 20:19:29 -0700

    change password functionality added

--- Banqi/frontend/src/api.js
+++ Banqi/frontend/src/api.js
@@ -14,6 +14,7 @@ export default {
   registerUser(userData) {
     return AXIOS.post('/user/add', userData);
   },
+
   getAllUsers() {
     return AXIOS.get('/user/list');
   },
@@ -22,11 +23,20 @@ export default {
   hello() {
     return AXIOS.get(`/game/hello`);
   },
+
   getExistingGame(gameId) {
     return AXIOS.get(`/game/${gameId}/board`);
   },
 
   getNewGame() {
     return AXIOS.get('/game/create');
+  },
+
+  createNewGame(id1, id2) {
+    return AXIOS.get(`/game/create/${id1}/${id2}`);
+  },
+
+  updateUser(userId, userInfo) {
+    return AXIOS.post(`/user/${userId}/update`, userInfo);
   }
 }
