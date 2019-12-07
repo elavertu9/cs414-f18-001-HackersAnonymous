1c57c13b5c2b206228fe25db612348de21a208ed
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-12-06 17:49:01 -0700

    Add rank column and reorder pieces in Rules modal

--- Banqi/frontend/src/components/BanqiRules.vue
+++ Banqi/frontend/src/components/BanqiRules.vue
@@ -2,48 +2,56 @@
     <b-container>
       <table class="table">
         <thead>
-        <tr class="center">
-          <td></td>
-          <td><h5>Name</h5></td>
-          <td><h5>Ability</h5></td>
-        </tr>
+          <tr class="center">
+            <td></td>
+            <td><h5>Rank</h5></td>
+            <td><h5>Name</h5></td>
+            <td><h5>Ability</h5></td>
+          </tr>
         </thead>
         <tbody>
-        <tr class="center">
-          <td><img src="../images/Pieces/White_Advisor.png"></td>
-          <td class="middle">Advisor</td>
-          <td class="middle">Blah</td>
-        </tr>
-        <tr class="center">
-          <td><img src="../images/Pieces/White_Soldier.png"></td>
-          <td class="middle">Soldier</td>
-          <td class="middle">Blah</td>
-        </tr>
-        <tr class="center">
-          <td><img src="../images/Pieces/White_General.png"></td>
-          <td class="middle">General</td>
-          <td class="middle">Blah</td>
-        </tr>
-        <tr class="center">
-          <td><img src="../images/Pieces/White_Elephant.png"></td>
-          <td class="middle">Elephant</td>
-          <td class="middle">Blah</td>
-        </tr>
-        <tr class="center">
-          <td><img src="../images/Pieces/White_Cannon.png"></td>
-          <td class="middle">Cannon</td>
-          <td class="middle">Blah</td>
-        </tr>
-        <tr class="center">
-          <td><img src="../images/Pieces/White_Chariot.png"></td>
-          <td class="middle">Chariot</td>
-          <td class="middle">Blah</td>
-        </tr>
-        <tr class="center">
-          <td><img src="../images/Pieces/White_Horse.png"></td>
-          <td class="middle">Horse</td>
-          <td class="middle">Blah</td>
-        </tr>
+          <tr class="center">
+            <td><img src="../images/Pieces/White_General.png"></td>
+            <td>7</td>
+            <td class="middle">General</td>
+            <td class="middle">Blah</td>
+          </tr>
+          <tr class="center">
+            <td><img src="../images/Pieces/White_Chariot.png"></td>
+            <td>6</td>
+            <td class="middle">Chariot</td>
+            <td class="middle">Blah</td>
+          </tr>
+          <tr class="center">
+            <td><img src="../images/Pieces/White_Horse.png"></td>
+            <td>5</td>
+            <td class="middle">Horse</td>
+            <td class="middle">Blah</td>
+          </tr>
+          <tr class="center">
+            <td><img src="../images/Pieces/White_Cannon.png"></td>
+            <td>4</td>
+            <td class="middle">Cannon</td>
+            <td class="middle">Blah</td>
+          </tr>
+          <tr class="center">
+            <td><img src="../images/Pieces/White_Advisor.png"></td>
+            <td>3</td>
+            <td class="middle">Advisor</td>
+            <td class="middle">Blah</td>
+          </tr>
+          <tr class="center">
+            <td><img src="../images/Pieces/White_Elephant.png"></td>
+            <td>2</td>
+            <td class="middle">Elephant</td>
+            <td class="middle">Blah</td>
+          </tr>
+          <tr class="center">
+            <td><img src="../images/Pieces/White_Soldier.png"></td>
+            <td>1</td>
+            <td class="middle">Soldier</td>
+            <td class="middle">Blah</td>
+          </tr>
         </tbody>
       </table>
     </b-container>
