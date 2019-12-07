20af8b7148294f1786c9c02193a1a112107678f5
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-12-06 18:42:15 -0700

    Finish up the 'How to play' section.

--- Banqi/frontend/src/components/BanqiRules.vue
+++ Banqi/frontend/src/components/BanqiRules.vue
@@ -1,9 +1,21 @@
 <template>
-    <b-container>
-      <h4><b>How to play</b></h4>
+    <p>
+      <h3><b>How to play</b></h3>
+      <br/>
+      <h5><b>Start</b></h5>
       <p> The first player begins by flipping any piece on the board over. the color of the first flipped over piece
       will be assigned to player 1, and player 2 will be assigned to the opposing team. Player 2 will then begin their
         first turn by flipping over any piece on the board.</p>
+      <br/>
+      <h5><b>Moving pieces</b></h5>
+      <p>All pieces can move up, down, left, and right as long as there is an empty square to move to,
+        or the piece that is occupying the square can be legally captured</p>
+      <br/>
+      <h5><b>Win conditions</b></h5>
+      <ul>
+        <li>A player successfully captures all of their opponents pieces.</li>
+        <li>A player can force their opponent into a position where they can no longer make any moves</li>
+      </ul>
       <table class="table table-hover">
         <thead>
           <tr class="center">
