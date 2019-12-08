6b82da6e46e2b4e73bc3c4c16337e8a7df920b21
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2019-12-07 22:33:23 -0700

    Updated the 'Moving pieces' section

--- Banqi/frontend/src/components/BanqiRules.vue
+++ Banqi/frontend/src/components/BanqiRules.vue
@@ -74,7 +74,8 @@
     <br/>
     <h5><b>Moving pieces</b></h5>
     <p>All pieces can move up, down, left, and right as long as there is an empty square to move to,
-      or the piece that is occupying the square can be legally captured.</p>
+      or the piece that is occupying the square can be legally captured. A piece must be flipped over before
+    it can be moved.</p>
     <br/>
     <h5><b>Win conditions</b></h5>
     <ul>
